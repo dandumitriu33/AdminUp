bb35ccca64e02575adf3c80d1bd7814652aef951
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-04 20:22:00 +0300

    Add DbContext set up in Library/DataAccess w/ sets

---
+++ AdminUp.DataAccessLibrary/DataAccess/AdminUpContext.cs
@@ -0,0 +1,19 @@
+﻿using AdminUp.DataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdminUp.DataAccessLibrary.DataAccess
+{
+    public class AdminUpContext : DbContext
+    {
+        public AdminUpContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Appartment> Appartments { get; set; }
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<Announcement> Announcements { get; set; }
+    }
+}
