c2d63a51196c63db9855f54628fd2bf930d8189d
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-05 14:09:30 +0300

    Add Register Navbar button

--- AdminUp/Models/RegisterViewModel.cs
+++ AdminUp/Models/RegisterViewModel.cs
@@ -9,8 +9,10 @@ namespace AdminUp.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
         [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         [Required]
         [EmailAddress]
