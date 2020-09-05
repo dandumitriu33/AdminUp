8aa6b8a06d74d61dacd0f41635ab605916e8f525
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-05 13:24:23 +0300

    Add Identity service in Startup

--- AdminUp/Startup.cs
+++ AdminUp/Startup.cs
@@ -7,6 +7,7 @@ using AdminUp.DataAccessLibrary.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,6 +31,7 @@ namespace AdminUp
             {
                 options.UseSqlServer(Configuration.GetConnectionString("Default"));
             });
+            services.AddIdentity<IdentityUser, IdentityRole>();
             services.AddControllersWithViews();
             services.AddScoped<IRepository, SQLRepository>();
         }
