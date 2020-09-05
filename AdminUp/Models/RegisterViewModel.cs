32878cad2f8404a15a05526781c1963f76cd9439
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-05 13:42:16 +0300

    Add a RegisterViewModel for the register acc view

---
+++ AdminUp/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminUp.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The Password and Confirm Password fields do not match.")]
+        public string PasswordConfirmation { get; set; }
+    }
+}
