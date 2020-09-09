e05f53c9edf9d1d3308b5462cb09af6cb9acd1ff
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-09 18:47:55 +0300

    Add CreateRole action Post method and form

--- AdminUp/Controllers/AdministrationController.cs
+++ AdminUp/Controllers/AdministrationController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AdminUp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,6 +9,8 @@ using System.Threading.Tasks;
 
 namespace AdminUp.Controllers
 {
+    [Authorize(Roles = "SuperAdmin")]
+
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
@@ -22,10 +26,29 @@ namespace AdminUp.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult CreateRole()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
     }
 }
